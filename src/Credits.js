3905936a12726d77de3787ec310f7d37fa622b18
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-08-27 23:45:29 +0500

    fix

--- src/Credits.js
+++ src/Credits.js
@@ -8,27 +8,14 @@ export default function Credits() {
     <div className="Credits">
       <p>By Abeer Ali Khan</p>
       <div className="Credits-socials">
-        <a
-          href="https://github.com/abeeralikhan"
-          target="_blank"
-          aria-label="Github"
-        >
+        <a href="https://github.com/abeeralikhan" aria-label="Github">
           <img src={githubIcon} alt="" />
         </a>
-        <a
-          href="https://twitter.com/abeerkh4n"
-          target="_blank"
-          aria-label="Twitter"
-        >
+        <a href="https://twitter.com/abeerkh4n" aria-label="Twitter">
           <img src={twitterIcon} alt="" />
         </a>
         <a href="https://linkedin.com/in/abeeralikhan">
-          <img
-            src={linkedInIcon}
-            alt=""
-            target="_blank"
-            aria-label="LinkedIn"
-          />
+          <img src={linkedInIcon} alt="" aria-label="LinkedIn" />
         </a>
       </div>
     </div>
